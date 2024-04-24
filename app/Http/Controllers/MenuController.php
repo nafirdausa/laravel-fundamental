c4198ed10a9adf4aafdd6e6965a0a1a6c44e3929
nafirdausa <bless.azz35@gmail.com>
2024-04-24 13:52:28 +0700

    none

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -11,18 +11,18 @@ class MenuController extends Controller
         // Daftar Pilihan Menu
         $NamaMenu = [
             [
-                "id" => 0,
-                "nama_menu" => "Soto Mie",
+                "id" => 1,
+                "makanan" => "Soto Mie",
                 "harga" => 8000,
             ],
             [
-                "id" => 1,
-                "nama_menu" => "Bakso Malang",
+                "id" => 2,
+                "makanan" => "Bakso Malang",
                 "harga" => 13000,
             ],
             [
-                "id" => 2,
-                "nama_menu" => "Ketoprak",
+                "id" => 3,
+                "makanan" => "Ketoprak",
                 "harga" => 15000,
             ],
             // ...
@@ -38,12 +38,12 @@ class MenuController extends Controller
 
         foreach ($NamaMenu as $menu) {
             // Ubah ke huruf kecil untuk pencarian yang tidak bersifat case sensitive
-            $lowercaseMenu = strtolower($menu['nama_menu']);
+            $lowercaseMenu = strtolower($menu['makanan']);
             $lowercaseSearchTerm = strtolower($PilihanMenu);
 
             // Periksa apakah kata kunci pencarian adalah substring dari nama menu
             if (strpos($lowercaseMenu, $lowercaseSearchTerm) !== false && $harga == $menu['harga']) {
-                return "Anda memilih makanan {$menu['nama_menu']} dengan harga {$menu['harga']}";
+                return "Anda memilih makanan {$menu['makanan']} dengan harga {$menu['harga']}";
             }
         }
         return "Menu tidak ditemukan";
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,7 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
-use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MenuController;
-use Inertia\Inertia;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,33 +9,10 @@ use Inertia\Inertia;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
-
-// Route::get('/', function () {
-//     return Inertia::render('Welcome', [
-//         'canLogin' => Route::has('login'),
-//         'canRegister' => Route::has('register'),
-//         'laravelVersion' => Application::VERSION,
-//         'phpVersion' => PHP_VERSION,
-//     ]);
-// });
-
-// Route::get('/dashboard', function () {
-//     return Inertia::render('Panel/Home');
-// })->middleware(['auth', 'verified'])->name('dashboard');
-
-// Route::get('/manage-user', function () {
-//     return Inertia::render('Panel/Manage-User');
-// })->middleware(['auth', 'verified'])->name('manage-user');
-
-// Route::middleware('auth')->group(function () {
-//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-// });
 
 Route::get('/', function () {
     return view('welcome');
@@ -51,5 +25,3 @@ Route::get('/list-menu', function () {
 });
 
 Route::get('/list-menu/{namaMenu}/{harga}', [MenuController::class, 'getAllData']);
-
-require __DIR__.'/auth.php';
